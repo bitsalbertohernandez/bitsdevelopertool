00d70972ad08e826e01532d59f7bd230e28e31b9
bitsalbertohernandez <alberto.hernandez@bitsamericas.com>
2019-06-21 09:42:41 -0500

    Estilos de código y eliminación de archivos que no se usan

--- src/Common/YAMLType.php
+++ src/Common/YAMLType.php
@@ -1,8 +1,7 @@
 <?php
 namespace Drupal\bits_developer_tool\Common;
 
-class YAMLType
-{
+class YAMLType {
   const INFO_FILE = 'info.yml';
   const LIBRARIES_FILE = 'libraries.yml';
   const ROUTING_FILE = 'routing.yml';
