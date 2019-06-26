b06e08b9fa26d34ba71ab86bca18378e85ab1e4b
Ernesto Gil <ernesto.gil@bitsaericas.com>
2019-06-26 08:48:07 -0400

    Chequeando que el id del servicio exista

--- src/Form/FormGeneratorForm.php
+++ src/Form/FormGeneratorForm.php
@@ -55,12 +55,12 @@ class FormGeneratorForm extends GenericGeneratorForm
       '#default_value' => '',
       '#description' => t("El identificador no debe contener espacios no caracteres extraños"),
     ];
-    $form['generator_container2'. $this->typeOfFile()]['integration']['form_class'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Nombre de la clase Formulario'),
-      '#default_value' => '',
-      '#description' => t("Clase formulario que instancia la clase logica seleccionada"),
-    ];
+//    $form['generator_container2'. $this->typeOfFile()]['integration']['form_class'] = [
+//      '#type' => 'textfield',
+//      '#title' => $this->t('Nombre de la clase Formulario'),
+//      '#default_value' => '',
+//      '#description' => t("Clase formulario que instancia la clase logica seleccionada"),
+//    ];
     $form['generator_container2'. $this->typeOfFile()]['integration_logic']['submit_method_integration_logic'] = [
       '#type' => 'checkbox',
       '#title' => t('Function submitForm'),
@@ -102,7 +102,6 @@ class FormGeneratorForm extends GenericGeneratorForm
       $methods[] = 'buildForm';
     if ($form_state->getValue('validate_method_integration_logic') == true)
       $methods[] = 'validateForm';
-    //ksm($class_integration, $module_int, $module_imp, $class_specific_logic, $service_int, $methods);
     $builder_controller = \Drupal::service('bits_developer.int-form.builder');
     //$builder_controller->addFormClass($form_class);
     $builder_controller->addLogicClass($class_specific_logic);
@@ -145,10 +144,20 @@ class FormGeneratorForm extends GenericGeneratorForm
   }
 
   private function validateFormIntegrationInput(array $form, FormStateInterface $form_state) {
-    $form_class = $form_state->getValue('form_class');
-    if ($form_class == '') {
-      $form_state->setErrorByName('form_class', $this->t('Debe introducir el nombre de la clase Formulario.'));
-    }
+
     parent::validateIntegrationInput($form_state);
+//    $form_class = $form_state->getValue('form_class');
+//    if ($form_class == '') {
+//      $form_state->setErrorByName('form_class', $this->t('Debe introducir el nombre de la clase Formulario.'));
+//    }
+    $service_int = $form_state->getValue('service_integration');
+    $module_int = $form_state->getValue('module_integration');
+    if ($service_int != '') {
+      $file_manager = \Drupal::service('bits_developer.file.manager');
+      $dir = $file_manager->modulePath($module_int, '').'/'.$module_int.'.services.yml';
+      $success = $file_manager->existKeyInYAMLFile($dir, $service_int);
+      if (!$success)
+        $form_state->setErrorByName('service_integration', $this->t('Debe introducir id valido para el servicio.'));
+    }
   }
 }
